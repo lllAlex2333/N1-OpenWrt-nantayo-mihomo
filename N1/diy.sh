c4256fb78d15a953ce58e180404d9c695f7c4bd6
lllAlex2333 <18554461@qq.com>
2024-12-02 18:12:02 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 #修改密码
-sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/^root:.*:/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # => alist
 rm -rf feeds/packages/lang/golang
